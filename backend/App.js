ba222e309e672c421ecb8da7f618efd486f85f1a
RamaniYagnik <ramaniyagnik06@gmail.com>
2025-08-12 17:19:48 +0530

    Again Backend updated successfully

--- backend/App.js
+++ backend/App.js
@@ -21,7 +21,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-     origin: [ process.env.FRONTEND_URL ],        
+     origin: [ "https://fullstackproject-gold.vercel.app" ],        
     credentials: true
   })
 );
